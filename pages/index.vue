a4bbdd48758633242d657e383083f1810c6e53ac
codebender828 <jbakebwa@163.com>
2019-05-18 13:26:26 +0800

    feat: yay! video

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,12 @@
 <template>
   <section class="container">
-    <video id="videoElement" ref="video" autoplay="true" />
+    <video autoplay="true" />
   </section>
 </template>
 
 <script lang="js">
+
 export default {
-  data() {
-    return {
-      stream: undefined
-    }
-  },
-  mounted() {
-    const video = this.$refs.video
-    // const canvas = document.querySelector('canvas')
-    // const ctx = canvas.getContext('2d')
-
-    /** @type {MediaStreamConstraints} */
-    const constraints = {
-      video: { facingMode: 'user' }
-    }
-
-    // Combine RTCPeerConnection with getUserMedia
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then((stream) => {
-        video.srcObject = stream
-
-        // peerConnection.addStream(stream)
-        // peerConnection.createOffer()
-        //   .then(sdp => peerConnection.setLocalDescription(sdp))
-        //   .then(() => {
-        //     socket.emit('offer', peerConnection.localDescription)
-        //   })
-        //   .catch(err => console.warn('[peerConnection]: Could not create offer', err))
-      })
-      .catch(err => console.error(err))
-  }
 }
 </script>
 
